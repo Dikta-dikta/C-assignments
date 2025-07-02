654b28bbab54cff5dc06948e2f711da6a0b887ed
Dikta <dikta0531@gmail.com>
2025-07-02 15:35:18 +0530

    added recursive funtion

--- reverse.c
+++ reverse.c
@@ -9,3 +9,11 @@ void reverseIterative(int arr[], int n) {
         i++; j--;
     }
 }
+void reverseRecursive(int arr[], int start, int end) {
+    if (start >= end)
+        return;
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    reverseRecursive(arr, start + 1, end - 1);
+}
